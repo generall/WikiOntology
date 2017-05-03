725fbc1b1c8770359a7447e088bb2a2f19dc019c
Andrey Vasnetsov <vasnetsov93@gmail.com>
2017-05-03 17:06:08 +0300

    fix concept weighting

--- src/main/scala/ml/generall/ontology/structure/Concept.scala
+++ src/main/scala/ml/generall/ontology/structure/Concept.scala
@@ -15,6 +15,6 @@ case class Concept(_url: String) {
 
   def loadRootCategories(url: String): List[(String, Double)] = {
     val (cats, weights) = SqliteClient.getCategoriesPerConcept(url).unzip
-    cats.zip(ProbTools.scaleArray(weights.map(w => Math.log(1.0 / w))))
+    cats.zip(ProbTools.scaleArray(weights.map(w => 1.0 / Math.log(1.0 / w))))
   }
 }
